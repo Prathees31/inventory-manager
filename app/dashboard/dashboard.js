157b90adca0573ade6a2e071ccf53d3d3464d263
Prathees31 <prathees.g.s@gmail.com>
2019-01-10 05:10:15 +0530

    Table Data ng repeat sample data

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -1,12 +1,49 @@
 'use strict';
 angular.module('im.dashboard',['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
-
     $routeProvider.when('/dashboard',{
         templateUrl: 'dashboard/dashboard.html',
         controller: 'dashboardCtrl'
     });
   }])
-.controller('dashboardCtrl',[function(){
+.controller('dashboardCtrl',['$scope',function($scope){
+        $scope.sampledata=[
+            {
+                "id": 1,
+                "ponum":"PO20190101001",
+                "supplier":"AGS",
+                "podate":"2019-01-01",
+                "status":"created",
+                "paymentstatus":"paid",
+                "delivery":"delivered"
+            },
+            {
+                "id": 2,
+                "ponum":"PO20190101002",
+                "supplier":"AGS",
+                "podate":"2019-01-01",
+                "status":"created",
+                "paymentstatus":"paid",
+                "delivery":"delivered"
+            },
+            {
+                "id": 3,
+                "ponum":"PO20190101003",
+                "supplier":"AGS",
+                "podate":"2019-01-01",
+                "status":"created",
+                "paymentstatus":"paid",
+                "delivery":"delivered"
+            },
+            {
+                "id": 4,
+                "ponum":"PO20190101004",
+                "supplier":"AGS",
+                "podate":"2019-01-01",
+                "status":"created",
+                "paymentstatus":"paid",
+                "delivery":"delivered"
 
+            }
+        ];
 }]);
